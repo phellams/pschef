55514155011d6489acaba084f0145a9f4a9a918d
sgkens <sgkens@gmail.com>
2026-02-28 22:59:38 +1100

    remove: removed pantry files and added to own repository

--- ingredients/rust/cargo.ps1
+++
@@ -1,6 +0,0 @@
-param([string]$Name)
-
-# 1. Fail Fast if cargo isn't found
-Assert-ChefTool -Command "cargo" -Message "Rust toolchain missing."
-
-Show-SousChef -Message "Scaffolding Rust..." -Current 20 -Total 100
--- recipes/DevLab.ps1
+++
@@ -1,8 +0,0 @@
-@{
-    Description = "Full Local Dev Environment (WSL + Docker + Postgres)"
-    Sequence    = @(
-        @{ Group = "sys"; Action = "wsl"; Params = @{ Distro = "Debian" } },
-        @{ Group = "sys"; Action = "docker"; Params = @{} },
-        @{ Group = "db"; Action = "postgres"; Params = @{ Name = "DevDB"; Storage = "./data" } }
-    )
-}
